84d0ed2cf3e00b2eafd1104d5316caf4db35f633
vitaliyozolin-dotcom <vitaliyozolin@gmail.com>
2026-09-24 06:56:10 -0700

    feat: activate product-first VELA homepage and accurate structured data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import IkiomaLanding from "@/components/IkiomaLanding";
+import VelaLanding from "@/components/VelaLanding";
 
 export default function Home() {
   const structuredData = {
@@ -6,40 +6,25 @@ export default function Home() {
     "@graph": [
       {
         "@type": "Organization",
+        "@id": "https://ikioma.ru/#organization",
         name: "ИКИОМА",
         url: "https://ikioma.ru",
-        description: "Современные дома из SIP-панелей с понятной ценой, сроком и видимым процессом.",
+        description: "Одноэтажные дома из SIP-панелей с согласованной комплектацией и поэтапной приёмкой.",
       },
       {
         "@type": "Product",
-        name: "Дом КОНТУР",
-        description: "Одноэтажный SIP-дом: 86,2 м² полезной площади, три спальни, два санузла и крытая терраса 23,1 м².",
+        name: "ИКИОМА | VELA",
+        brand: { "@type": "Brand", name: "ИКИОМА" },
+        description: "Одноэтажный SIP-дом: три спальни, два санузла, 86,2 м² внутри и крытая терраса 23,1 м². Цены комплектаций являются предварительными ориентирами; земля и условия участка рассчитываются отдельно.",
         image: "https://ikioma.ru/images/kontur-family-exterior-v1.webp",
         additionalProperty: [
-          { "@type": "PropertyValue", name: "Площадь застройки", value: "120 м²" },
-          { "@type": "PropertyValue", name: "Внешний контур дома", value: "96 м²" },
-          { "@type": "PropertyValue", name: "Полезная площадь", value: "86,2 м²" },
-          { "@type": "PropertyValue", name: "Терраса", value: "23,1 м²" },
+          { "@type": "PropertyValue", name: "Полезная площадь помещений", value: "86,2 м²" },
+          { "@type": "PropertyValue", name: "Крытая терраса, отдельно от помещений", value: "23,1 м²" },
+          { "@type": "PropertyValue", name: "Спальни в опубликованном проекте", value: "3" },
+          { "@type": "PropertyValue", name: "Санузлы", value: "2" },
         ],
-        offers: {
-          "@type": "AggregateOffer",
-          priceCurrency: "RUB",
-          lowPrice: "5200000",
-          highPrice: "7200000",
-          offerCount: "3",
-          url: "https://ikioma.ru/#finance",
-        },
       },
     ],
   };
-
-  return (
-    <>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-      />
-      <IkiomaLanding />
-    </>
-  );
+  return <><script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData).replace(/</g, "\\u003c") }} /><VelaLanding /></>;
 }
